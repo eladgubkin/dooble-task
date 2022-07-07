bb69953e64dbbace2b3d85fe30c4fd5c773529a5
Elad Gubkin <Eladox3@gmail.com>
2022-07-07 17:24:40 +0300

    @MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Button from "@mui/material/Button";
+
 export const App = () => {
   return (
     <div className="App">
       <h1>App</h1>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 };

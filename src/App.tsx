c188f8e27c0349b13a2e3e927a5bb3da71775511
Elad Gubkin <Eladox3@gmail.com>
2022-07-09 23:55:56 +0300

    App supports search and filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import { useState } from "react";
 import { Table } from "./views/Table/Table";
 import { useRickAndMorty } from "./hooks/useRickAndMorty";
-import Layout from "./components/Layout";
+import { Layout } from "./components/layout/Layout";
+import { useWindowSize } from "./hooks/useWindowSize";
+import { Controls } from "./components/common/Controls";
+import { Loading } from "./components/common/Loading";
+
+const responsiveStyle = { height: "calc(100vh - 200px)", width: "100%", marginTop: 56 };
+const normalStyle = { height: "calc(100vh - 208px)", width: "100%", marginTop: 64 };
 
 export const App = () => {
   const [pageNumber, setPageNumber] = useState(0);
+  const [search, setSearch] = useState("");
+  const [status, setStatus] = useState("");
+  const [gender, setGender] = useState("");
+  const { width } = useWindowSize();
+  const { isLoading, data } = useRickAndMorty(pageNumber, search, status, gender);
 
-  const { isLoading, data, isError } = useRickAndMorty(pageNumber);
-
-  if (isError) return <h1>Error...</h1>;
-  if (isLoading) return <h1>Loading...</h1>;
+  if (isLoading) return <Loading />;
 
   return (
     <Layout>
-      <Table
-        data={data.results}
-        rowCount={data.info.count}
-        pageCount={data.info.pages}
-        isLoading={isLoading}
-        pageNumber={pageNumber}
-        setPageNumber={setPageNumber}
-      />
+      <div style={width < 600 ? responsiveStyle : normalStyle}>
+        <Controls
+          search={search}
+          setSearch={setSearch}
+          status={status}
+          setStatus={setStatus}
+          gender={gender}
+          setGender={setGender}
+        />
+        <Table data={data} isLoading={isLoading} setPageNumber={setPageNumber} />
+      </div>
     </Layout>
   );
 };

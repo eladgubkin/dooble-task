67e9357e260b4ccbd857ebdd78b1acb46c9cfc2e
Elad Gubkin <Eladox3@gmail.com>
2022-07-09 23:54:20 +0300

    Move into common folder

---
+++ src/components/common/SearchBar.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { TextField } from "@mui/material";
+import { useDebounce } from "../../hooks/useDebounce";
+
+export const SearchBar = ({ search, setSearch }) => {
+  const [value, setValue] = useState("");
+  const debouncedSearch = useDebounce(value, 500);
+
+  useEffect(() => {
+    setSearch(debouncedSearch);
+  }, [debouncedSearch]);
+
+  useEffect(() => {
+    if (search === "") setValue("");
+  }, [search]);
+
+  return (
+    <TextField
+      fullWidth
+      label="Search characters..."
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+};

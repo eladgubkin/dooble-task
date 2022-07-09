99fa9d81645303bda2cf10c9ac8d9a1cc80c4b10
Elad Gubkin <Eladox3@gmail.com>
2022-07-09 23:37:43 +0300

    Add option for dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { App } from "./App";
 import "./main.css";
 
 export const queryClient = new QueryClient();
 
+const theme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );

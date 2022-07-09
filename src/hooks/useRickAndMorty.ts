d4eb1b0bb416a11ca4a2b6fbf5df041204b22983
Elad Gubkin <Eladox3@gmail.com>
2022-07-09 23:53:54 +0300

    Update useRickAndMorty to support search and filters

--- src/hooks/useRickAndMorty.ts
+++ src/hooks/useRickAndMorty.ts
@@ -2,21 +2,33 @@ import { useEffect } from "react";
 import { useQuery } from "react-query";
 import { queryClient } from "../main";
 
-const fetchCharacters = (pageNumber: number) =>
-  fetch("https://rickandmortyapi.com/api/character?page=" + (pageNumber + 1)).then((res) =>
-    res.json()
-  );
+const fetchCharacters = (pageNumber: number, search: string, status: string, gender: string) =>
+  fetch(
+    `https://rickandmortyapi.com/api/character?page=${
+      pageNumber + 1
+    }&name=${search}&status=${status}&gender=${gender}`
+  ).then((res) => res.json());
 
-export const useRickAndMorty = (pageNumber: number) => {
+export const useRickAndMorty = (
+  pageNumber: number,
+  search: string,
+  status: string,
+  gender: string
+) => {
   useEffect(() => {
     queryClient.prefetchQuery(
-      ["characters", pageNumber + 1],
-      () => fetchCharacters(pageNumber + 1),
+      ["characters", pageNumber + 1, search, status, gender],
+      () => fetchCharacters(pageNumber + 1, search, status, gender),
       { staleTime: 60 * 1000 }
     );
-  }, [pageNumber]);
+  }, [pageNumber, search]);
 
-  return useQuery(["characters", pageNumber], () => fetchCharacters(pageNumber), {
-    staleTime: 60 * 1000,
-  });
+  return useQuery(
+    ["characters", pageNumber, search, status, gender],
+    () => fetchCharacters(pageNumber, search, status, gender),
+    {
+      staleTime: 60 * 1000,
+      keepPreviousData: true,
+    }
+  );
 };

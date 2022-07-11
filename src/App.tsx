4546a1a9bdebfc495fb19f8580f31cb5183d7694
Elad Gubkin <Eladox3@gmail.com>
2022-07-11 11:50:49 +0300

    Add state for Dialog open and close

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Layout } from "./components/layout/Layout";
 import { useWindowSize } from "./hooks/useWindowSize";
 import { Controls } from "./components/common/Controls";
 import { Loading } from "./components/common/Loading";
+import { Dialog } from "./components/common/Dialog";
 
 const responsiveStyle = { height: "calc(100vh - 200px)", width: "100%", marginTop: 56 };
 const normalStyle = { height: "calc(100vh - 208px)", width: "100%", marginTop: 64 };
@@ -16,6 +17,8 @@ export const App = () => {
   const [gender, setGender] = useState("");
   const { width } = useWindowSize();
   const { isLoading, data } = useRickAndMorty(pageNumber, search, status, gender);
+  const [character, setCharacter] = useState(null);
+  const [open, setOpen] = useState(false);
 
   if (isLoading) return <Loading />;
 
@@ -30,8 +33,17 @@ export const App = () => {
           gender={gender}
           setGender={setGender}
         />
-        <Table data={data} isLoading={isLoading} setPageNumber={setPageNumber} />
+
+        <Table
+          data={data}
+          isLoading={isLoading}
+          setPageNumber={setPageNumber}
+          setCharacter={setCharacter}
+          handleOpen={() => setOpen(true)}
+        />
       </div>
+
+      {character && <Dialog open={open} handleClose={() => setOpen(false)} character={character} />}
     </Layout>
   );
 };

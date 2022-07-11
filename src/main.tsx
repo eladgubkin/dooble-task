bfaee5a1483714f34100bbacff1cc1babbf0569d
Elad Gubkin <Eladox3@gmail.com>
2022-07-11 22:18:22 +0300

    Add dark | light mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,51 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import React, { useMemo, createContext, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { App } from "./App";
 import "./main.css";
 
+export const ColorModeContext = createContext({ toggleColorMode: () => {} });
 export const queryClient = new QueryClient();
 
-const theme = createTheme({
-  palette: {
-    mode: "light",
-  },
-});
+const Main = () => {
+  const [mode, setMode] = useState<"light" | "dark">("light");
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline>
+          <App />
+        </CssBaseline>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
+      <Main />
     </QueryClientProvider>
   </React.StrictMode>
 );
